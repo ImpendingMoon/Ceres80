6d7ca70c8829e13e6cf9f4932257440d6cfc4b94
ImpendingMoon <theimpendingmoon@gmail.com>
2025-02-09 18:26:34 -0600

    Fix i_lcd_wait
    - Fixed bug where B was not preserved
    - Moved wait loop to top of routine

--- Source/BIOS/display.s
+++ Source/BIOS/display.s
@@ -5,16 +5,17 @@
 ; Returns: None
 ;**
 i_lcd_wait:
+    PUSH BC
+.lcd_wait_loop:
+    LD B, 17                            ; Wait ~37us for LCD
+    DJNZ $
+
     IN A, (LCD_C1)
     RLCA
     JR C, .lcd_wait_loop
     IN A, (LCD_C2)
     RLCA
     JR C, .lcd_wait_loop
+    
+    POP BC
     RET
-
-.lcd_wait_loop:
-    LD B, 17                            ; Wait ~37us for LCD
-    DJNZ $
-    JR i_lcd_wait
-
